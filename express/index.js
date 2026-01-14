6f1f168bdc6f6d1fa3f04605a5bd6bdb2dffffa2
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-14 18:25:48 -0500

    feat: returning json - health

--- express/index.js
+++ express/index.js
@@ -8,6 +8,10 @@ app.get("/", (request, respond) => {
   respond.send("Hello World!");
 });
 
+app.get("/health", (request, response) => {
+  response.json({ status: "ok", uptime: process.uptime() });
+});
+
 // Starting the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
