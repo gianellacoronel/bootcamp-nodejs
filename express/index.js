0e3425899eecb27975b12fa0d2bee624a0e31c1d
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-14 18:33:11 -0500

    feat: add middleware with USE

--- express/index.js
+++ express/index.js
@@ -3,6 +3,15 @@ import express from "express";
 const PORT = process.env.PORT || 1234;
 const app = express();
 
+//This is a middleware, in every route, it will be executed before the route handler
+app.use((request, respond, next) => {
+  const timeString = new Date().toLocaleTimeString();
+  console.log(`[${timeString}] ${request.method} ${request.url}`);
+
+  // We NEED to put its NEXT step
+  next();
+});
+
 // Defining the route
 app.get("/", (request, respond) => {
   respond.send("Hello World!");
